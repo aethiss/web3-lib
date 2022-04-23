4d4f5b14811fb7fb630dde524fb70c0d5200e61a
Enzo D'Onofrio <aethiss@Enzos-MBP.fritz.box>
2022-04-23 11:00:40 +0200

    load lib when dom is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = {
 	test: () => { console.log("Hello World !") }
 };
+
+(() => {
+	if (!document) return;
+	document.addEventListener('DOMContentLoaded', () => {
+		console.log("Document Ready")
+		const walletButton = document.getElementById("wallet-connect");
+		if (!walletButton) {
+			console.log("No Wallet Button Found !");
+		} else {
+			console.log("Found Wallet Button");
+		}
+	})
+})()
