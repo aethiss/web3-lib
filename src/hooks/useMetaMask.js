0cbc2808208dcb5719b66a27cd735c5546eb775e
Enzo D'Onofrio <aethiss@Enzos-MacBook-Pro.fritz.box>
2022-04-24 18:22:34 +0200

    Added web3 lib to interact with wallet

--- src/hooks/useMetaMask.js
+++ src/hooks/useMetaMask.js
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from "react";
 
+import Web3 from "web3";
+
 const useMetaMask = () => {
 	const { ethereum } = window;
 
 	const [account, setAccount] = useState(null);
 	const [network, setNetwork] = useState(null);
+
+	const accountChangeListener = () => {
+		ethereum.on("accountsChanged", (accounts) => {
+			console.log("account changed");
+			// MetaMask Disconnected
+			if (!accounts.length) {
+				setAccount(null);
+				setNetwork(null);
+			}
+		});
+	}
 
 	const connectMetaMask = async () => {
 		console.log("connectMetaMask")
@@ -21,12 +34,25 @@ const useMetaMask = () => {
 				console.log("connected metamask", accounts, networkId);
 			} catch (e) {
 				console.log("Error on connectWithMetaMask", e);
-				// Clean Wrapper Status
 				setAccount(null);
 				setNetwork(null);
 			}
 		}
 	}
+
+	useEffect(() => {
+		if (ethereum) {
+			(async function () {
+				console.log("inside function")
+				const web3Instance = new Web3(ethereum);
+				const accounts = await web3Instance.eth.getAccounts();
+				const network = await web3Instance.eth.getChainId();
+				setAccount(accounts[0]);
+				setNetwork(network);
+			})()
+			accountChangeListener();
+		}
+	}, [])
 
 	return { account, network, connectMetaMask }
 }
